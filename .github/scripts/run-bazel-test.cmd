d3dffecc09dec732503aaa5c9696fa3723c8529e
Asyncly <asyncly@logmein.com>
2020-07-24 12:30:29 +0000

    Project import generated by Copybara.
    GitOrigin-RevId: 5dbaf0b0552666d0d21a4f72badad9b2635f57f1

--- .github/scripts/run-bazel-test.cmd
+++
@@ -1,5 +0,0 @@
-set "BAZEL_VS=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise"
-
-bazel test --test_output=all //... || EXIT /B 1
-
-bazel run -c opt //Test/Performance/... || EXIT /B 1
