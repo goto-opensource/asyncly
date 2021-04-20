91cadbab5d67108809084ad6511ec1b89d5c07a2
Asyncly <asyncly@logmein.com>
2021-04-20 07:37:01 +0000

    Project import generated by Copybara.
    GitOrigin-RevId: f0fd6e495ae768ac0ab2c54a2ff4857b543674ae

---
+++ Include/asyncly/future/BlockingWait.h
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2021 LogMeIn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include "asyncly/executor/InlineExecutor.h"
+#include "asyncly/future/Future.h"
+
+#include <future>
+
+namespace asyncly {
+template <typename F> using FutureReturnValueType = typename std::result_of<F()>::type::value_type;
+
+namespace detail {
+template <typename F, typename R = void> struct BlockingWait;
+
+template <typename F>
+struct BlockingWait<
+    F,
+    typename std::enable_if<!std::is_same<void, FutureReturnValueType<F>>::value, void>::type> {
+    FutureReturnValueType<F> operator()(const std::shared_ptr<IExecutor>& executor, const F& func)
+    {
+        using T = FutureReturnValueType<F>;
+        std::promise<T> syncPromise;
+        auto syncFuture = syncPromise.get_future();
+
+        executor->post([&func, &syncPromise]() mutable {
+            func()
+                .then([&syncPromise](T result) { syncPromise.set_value(std::move(result)); })
+                .catch_error(
+                    [&syncPromise](std::exception_ptr e) { syncPromise.set_exception(e); });
+        });
+
+        return syncFuture.get();
+    }
+};
+
+template <typename F>
+struct BlockingWait<
+    F,
+    typename std::enable_if<std::is_same<void, FutureReturnValueType<F>>::value, void>::type> {
+    FutureReturnValueType<F> operator()(const std::shared_ptr<IExecutor>& executor, const F& func)
+    {
+        std::promise<void> syncPromise;
+        auto syncFuture = syncPromise.get_future();
+
+        executor->post([&func, &syncPromise]() mutable {
+            func()
+                .then([&syncPromise]() { syncPromise.set_value(); })
+                .catch_error(
+                    [&syncPromise](std::exception_ptr e) { syncPromise.set_exception(e); });
+        });
+
+        syncFuture.get();
+    }
+};
+}
+
+/// Blockingly waits in the calling thread for the Future returning function to be executed in the
+/// given executor.
+/// \return the value the future resolves to. In case the future is rejected, the
+/// exception it is rejected with is thrown.
+/// Note: You should NEVER use this from within an asyncly executor because it may lead to a
+/// deadlock! Main use case is for interop between a single-threaded piece of code (e.g. `main`) and
+/// an asynchronous function.
+template <typename F>
+FutureReturnValueType<F> blocking_wait(const std::shared_ptr<IExecutor>& executor, const F& func)
+{
+    return detail::BlockingWait<F>{}(executor, func);
+}
+
+/// Blockingly waits in the calling thread for the given Future.
+/// \return the value the future resolves to. In case the future is rejected, the exception it
+/// is rejected with is thrown. Note: You should NEVER use this from within an asyncly executor
+/// because it may lead to a deadlock! Main use case is for interop between a single-threaded
+/// piece of code (e.g. `main`) and an asynchronous function (e.g. Proxy method).
+template <typename T> T blocking_wait(asyncly::Future<T>&& future);
+
+template <typename T> T blocking_wait(asyncly::Future<T>&& future)
+{
+    const auto executor = InlineExecutor::create();
+    std::promise<T> syncPromise;
+    auto syncFuture = syncPromise.get_future();
+
+    executor->post([&future, &syncPromise]() mutable {
+        future.then([&syncPromise](T result) { syncPromise.set_value(std::move(result)); })
+            .catch_error([&syncPromise](std::exception_ptr e) { syncPromise.set_exception(e); });
+    });
+
+    return syncFuture.get();
+}
+
+template <> inline void blocking_wait<>(asyncly::Future<void>&& future)
+{
+    const auto executor = InlineExecutor::create();
+    std::promise<void> syncPromise;
+    auto syncFuture = syncPromise.get_future();
+
+    executor->post([&future, &syncPromise]() mutable {
+        future.then([&syncPromise]() { syncPromise.set_value(); })
+            .catch_error([&syncPromise](std::exception_ptr e) { syncPromise.set_exception(e); });
+    });
+
+    syncFuture.get();
+}
+}
---
+++ Test/Unit/future/BlockingWait.cpp
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2021 LogMeIn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "asyncly/future/BlockingWait.h"
+#include "asyncly/executor/IExecutor.h"
+
+#include "StrandImplTestFactory.h"
+#include "asyncly/test/ExecutorTestFactories.h"
+
+#include "gmock/gmock.h"
+
+namespace asyncly {
+
+using namespace testing;
+
+template <typename TExecutorFactory> class BlockingWaitTest : public Test {
+  public:
+    BlockingWaitTest()
+        : factory_(std::make_unique<TExecutorFactory>())
+        , executor_(factory_->create())
+    {
+    }
+
+    std::unique_ptr<TExecutorFactory> factory_;
+    std::shared_ptr<IExecutor> executor_;
+};
+
+using ExecutorFactoryTypes = ::testing::Types<
+    asyncly::test::AsioExecutorFactory<>,
+    asyncly::test::DefaultExecutorFactory<>,
+    asyncly::test::StrandImplTestFactory<>>;
+
+TYPED_TEST_SUITE(BlockingWaitTest, ExecutorFactoryTypes);
+
+TYPED_TEST(BlockingWaitTest, shouldPostAndWaitForTaskReturningVoidFuture)
+{
+    std::atomic<bool> check = false;
+    const auto slowTask = [&check]() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
+        check = true;
+        return make_ready_future();
+    };
+
+    blocking_wait(this->executor_, slowTask);
+
+    EXPECT_TRUE(check);
+}
+
+TYPED_TEST(BlockingWaitTest, shouldPostAndWaitForTaskReturningNonVoidFuture)
+{
+    std::atomic<bool> check = false;
+    const auto slowTask = [&check]() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
+        check = true;
+        return make_ready_future(true);
+    };
+
+    EXPECT_TRUE(blocking_wait(this->executor_, slowTask));
+
+    EXPECT_TRUE(check);
+}
+
+TYPED_TEST(BlockingWaitTest, shouldWaitForVoidFuture)
+{
+    std::atomic<bool> check = false;
+    auto promise = std::make_shared<Promise<void>>();
+    this->executor_->post_after(std::chrono::milliseconds(20), [&check, promise]() {
+        check = true;
+        promise->set_value();
+    });
+
+    blocking_wait(promise->get_future());
+
+    EXPECT_TRUE(check);
+}
+
+TYPED_TEST(BlockingWaitTest, shouldWaitForNonVoidFuture)
+{
+    std::atomic<bool> check = false;
+    auto promise = std::make_shared<Promise<bool>>();
+    this->executor_->post_after(std::chrono::milliseconds(20), [&check, promise]() {
+        check = true;
+        promise->set_value(true);
+    });
+
+    EXPECT_TRUE(blocking_wait(promise->get_future()));
+
+    EXPECT_TRUE(check);
+}
+}
